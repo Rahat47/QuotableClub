f5e1ebe25c5400b09e5389380baf7d7b4c55a857
Rahat47 <djrayhan8@gmail.com>
2022-05-10 21:15:26 +0600

    updated footer button

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,12 +1,12 @@
 const navigation = {
     main: [
         {
-            name: 'Join The DaVicci Family Creed',
+            name: 'DaVicci Family Creed',
             href: 'https://www.facebook.com/groups/671163734219065/',
         },
         { name: 'Check Out FNB', href: 'https://direct.me/fnbooster' },
         {
-            name: 'See The Latest Vinni Divine',
+            name: 'Latest Vinni Divine',
             href: 'https://solo.to/vinnidavicci',
         },
     ],
@@ -24,11 +24,20 @@ const Footer = () => {
                         <div key={item.name} className='px-5 py-4'>
                             <a
                                 href={item.href}
-                                className='rounded-xl border border-red-700 bg-red-500 p-4 text-lg text-white transition-colors hover:bg-red-600 hover:text-black md:p-6'
-                                target='_blank'
-                                rel='noopener noreferrer'
+                                className='group relative inline-flex items-center justify-center overflow-hidden rounded-md px-6 py-3 font-bold text-white shadow-2xl'
                             >
-                                {item.name}
+                                <span className='absolute inset-0 h-full w-full bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 opacity-0 transition duration-300 ease-out group-hover:opacity-100' />
+                                {/* Top glass gradient */}
+                                <span className='absolute top-0 left-0 h-1/3 w-full bg-gradient-to-b from-white to-transparent opacity-5' />
+                                {/* Bottom gradient */}
+                                <span className='absolute bottom-0 left-0 h-1/3 w-full bg-gradient-to-t from-white to-transparent opacity-5' />
+                                {/* Left gradient */}
+                                <span className='absolute bottom-0 left-0 h-full w-4 bg-gradient-to-r from-white to-transparent opacity-5' />
+                                {/* Right gradient */}
+                                <span className='absolute bottom-0 right-0 h-full w-4 bg-gradient-to-l from-white to-transparent opacity-5' />
+                                <span className='absolute inset-0 h-full w-full rounded-md border border-white opacity-10' />
+                                <span className='absolute h-0 w-0 rounded-full bg-white opacity-5 transition-all duration-300 ease-out group-hover:h-56 group-hover:w-56' />
+                                <span className='relative'>{item.name}</span>
                             </a>
                         </div>
                     ))}
